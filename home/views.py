28b38f4fd921f2a4c0289bc7bcbf264186b423a6
Crowdbotics <team@crowdbotics.com>
2019-05-24 17:10:01 +0000

    add package django-bootstrap4

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
     ]
     context = {
         'customtext': CustomText.objects.first(),
